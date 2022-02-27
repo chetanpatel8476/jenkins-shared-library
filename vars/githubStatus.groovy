98a38a4827db1b5a725140eab171a25f0c39a427
chetanpatel8476 <chetanpatel671@gmail.com>
2022-02-27 05:34:42 +0530

    Added Github Status Verification Groovy

---
+++ vars/githubStatus.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.URL
+import java.net.URLConnection
+
+def call(String buildStatus = 'STARTED') {
+    println " buildstatus is ${buildStatus}"
+  //buildStatus =  buildStatus ?: 'succes'
+    if (buildStatus == 'STARTED') {
+        ghstatus = 'pending'
+    } else if (buildStatus == 'SUCCESS') {
+    // build status of null means successful
+        ghstatus = 'success'
+    } else {
+        ghstatus = 'error'
+    }
+
+    gh_api_endpoint = 'https://github.com/chetanpatel8476'
+    repos = ("${GIT_URL}" =~ /.*:(.+)\.git/)[ 0 ][ 1 ]
+    println "extracted repos string is ${repos}"
+    status_url = "${gh_api_endpoint}/repos/${repos}/statuses/${GIT_COMMIT}"
+    msg = """
+        { "state": "${ghstatus}",
+        "target_url": "${BUILD_URL}" }
+    """
+    httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: "${msg}", url: "${status_url}"
+}
